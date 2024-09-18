6737aa2ec3815e9a8886c263940df47d0cc4067f
kenibeze <kenibeze@gmail.com>
2024-09-18 08:49:02 -0400

    test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,9 +3,8 @@ import org.junit.jupiter.api.Test;
 public class LoginTest {
     @Test
     public void loginTest(){
-    public void loginTest1(){
-    public void loginTest2(){
-    public void loginTest3(){
+    public void loginTest1() {
+        }
 
     }
 }
